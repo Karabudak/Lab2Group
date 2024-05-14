24c28b99963488e05dbe857c2e6c4803dcbaa04c
Crhistian Vera Torres <elveraco001@hotmail.com>
2024-05-14 17:34:34 -0400

    Update Student.java
    updated student class as professor did

--- src/main/java/org/example/week2/Student.java
+++ src/main/java/org/example/week2/Student.java
@@ -1,43 +1,72 @@
 package org.example.week2;
+import javafx.fxml.FXML;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Student {
-    private String name;
-    private String surname;
-    private String id;
-    private String major;
+    @FXML
+    private StringProperty name;
+    @FXML
+    private StringProperty surname;
+    @FXML
+    private StringProperty id;
+    @FXML
+    private StringProperty major;
+
+    public Student(String name, String surname, String id, String major) {
+        this.name = new SimpleStringProperty(name);
+        this.surname = new SimpleStringProperty(surname);
+        this.id = new SimpleStringProperty(id);
+        this.major = new SimpleStringProperty(major);
+
+    }
 
     public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
         return name;
     }
+
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
+
     public String getSurname() {
+        return surname.get();
+    }
+
+    public StringProperty surnameProperty() {
         return surname;
     }
+
     public void setSurname(String surname) {
-        this.surname = surname;
+        this.surname.set(surname);
     }
+
     public String getId() {
+        return id.get();
+    }
+
+    public StringProperty idProperty() {
         return id;
     }
 
     public void setId(String id) {
-        this.id = id;
+        this.id.set(id);
     }
 
     public String getMajor() {
+        return major.get();
+    }
+
+    public StringProperty majorProperty() {
         return major;
     }
 
     public void setMajor(String major) {
-        this.major = major;
-    }
-
-    public Student(String name, String surname, String id, String major) {
-        this.name = name;
-        this.surname = surname;
-        this.id = id;
-        this.major = major;
+        this.major.set(major);
     }
 }
+
